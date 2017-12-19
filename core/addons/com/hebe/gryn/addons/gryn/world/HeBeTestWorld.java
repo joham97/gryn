2250842616405f7c6d67f018eabd9fd80f4c5056
joham97 <joham97@web.de>
2017-12-19 22:16:53 +0100

    Load Map From File

--- core/addons/com/hebe/gryn/addons/gryn/world/HeBeTestWorld.java
+++ core/addons/com/hebe/gryn/addons/gryn/world/HeBeTestWorld.java
@@ -1,5 +1,11 @@
 package com.hebe.gryn.addons.gryn.world;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import com.badlogic.gdx.Gdx;
 import com.hebe.gryn.addons.gryn.GrynAddonTextures;
 import com.hebe.gryn.logic.World;
 import com.hebe.gryn.logic.entity.tile.OffsetTile;
@@ -12,28 +18,31 @@ public class HeBeTestWorld {
 	public HeBeTestWorld(World world) {
 		this.world = world;
 		
-		for(int x = 0; x < 30; x++) {
-			for(int y = 0; y < 2; y++) {
-				this.world.getLayer(1).add(new OffsetTile(x, y, GrynAddonTextures.ASPHALT.getTextureID(), 6, 6));
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(Gdx.files.internal("../core/assets/world.txt").file()));
+			String line = null;
+			int y = 0;
+			while((line = br.readLine()) != null) {
+				int x = 0;
+				for(String tile : line.split(",")) {
+					if(tile.equals("1")) {
+						this.world.getLayer(0).add(new Tile(x, y, GrynAddonTextures.GRASS.getTextureID()));
+					}else if(tile.equals("2")) {
+						this.world.getLayer(0).add(new Tile(x, y, GrynAddonTextures.WATER.getTextureID()));
+					}else if(tile.equals("3")) {
+						this.world.getLayer(1).add(new OffsetTile(x, y, GrynAddonTextures.SAND.getTextureID(), 6, 6));
+					}else if(tile.equals("4")) {
+						this.world.getLayer(1).add(new OffsetTile(x, y, GrynAddonTextures.ASPHALT.getTextureID(), 6, 6));
+					}
+					x++;
+				}
+				y++;
 			}
-		}
-		
-		for(int x = 0; x < 30; x++) {
-			for(int y = 2; y < 7; y++) {
-				this.world.getLayer(0).add(new Tile(x, y, GrynAddonTextures.GRASS.getTextureID()));
-			}
-		}
-		
-		for(int x = 0; x < 10; x++) {
-			for(int y = 7; y < 14; y++) {
-				this.world.getLayer(0).add(new Tile(x, y, GrynAddonTextures.GRASS.getTextureID()));
-			}
-		}
-		
-		for(int x = 10; x < 22; x++) {
-			for(int y = 7; y < 14; y++) {
-				this.world.getLayer(1).add(new Tile(x, y, GrynAddonTextures.WATER.getTextureID()));
-			}
+			br.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 }
