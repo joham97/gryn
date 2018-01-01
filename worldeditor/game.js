b38ba8b423bbaf329655cebf2b1fa1b0719829c4
joham97 <joham97@web.de>
2018-01-01 14:17:09 +0100

    Import | Draw

--- worldeditor/game.js
+++ worldeditor/game.js
@@ -2,18 +2,76 @@ var grid = [];
 
 var reader;
 
-function setup() {   	
+var imgs = [];
+var higher = [];
+var offset = [];
+
+var loaded = false;
+
+var camX = 0;
+var camY = 0;
+
+function preload() {
+	imgs[1] = loadImage("../core/assets/tiles/grasstile.png");
+	higher[1] = false;
+	offset[1] = false;
+	imgs[2] = loadImage("../core/assets/tiles/watertile.png");
+	higher[2] = false;
+	offset[2] = false;
+	imgs[3] = loadImage("../core/assets/tiles/sandtile.png");
+	higher[3] = true;
+	offset[3] = true;
+	imgs[4] = loadImage("../core/assets/tiles/asphalt.png");
+	higher[4] = true;
+	offset[4] = true;
+}
+
+function setup() {
+	this.camX = 0;
+	this.camY = 0;
+
 	reader = new FileReader();
 	reader.onload = function (progressEvent) {
 		importClassDiagramFromJSON(this.result);
 	};
 
+	for (let x = 0; x < grid.length; x++) {
+
+	}
+
 	createCanvas(1200, 700);
 }
 
-function draw() {	
+function draw() {
 	background(200);
-
+	if (this.loaded) {
+		let centerTileX = floor(camX / 32);
+		let centerTileY = floor(camY / 32);
+		for (let x = max(0, centerTileX); x < min(grid.length, centerTileX + 40); x++) {
+			for (let y = max(0, centerTileY); y < min(grid[x].length, centerTileY + 40); y++) {
+				if (!this.higher[grid[x][y]]) {
+					if (this.offset[grid[x][y]]) {
+						image(imgs[grid[x][y]], (x - centerTileX) * 32 - 12, (y - centerTileY) * 32 - 12, 32, 32);
+					}
+					if (!this.offset[grid[x][y]]) {
+						image(imgs[grid[x][y]], (x - centerTileX) * 32, (y - centerTileY) * 32, 32, 32);
+					}
+				}
+			}
+		}
+		for (let x = max(0, centerTileX); x < min(grid.length, centerTileX + 40); x++) {
+			for (let y = max(0, centerTileY); y < min(grid[x].length, centerTileY + 40); y++) {
+				if (this.higher[grid[x][y]]) {
+					if (this.offset[grid[x][y]]) {
+						image(imgs[grid[x][y]], (x - centerTileX) * 32 - 12, (y - centerTileY) * 32 - 12, 56, 56);
+					}
+					if (!this.offset[grid[x][y]]) {
+						image(imgs[grid[x][y]], (x - centerTileX) * 32, (y - centerTileY) * 32, 56, 56);
+					}
+				}
+			}
+		}
+	}
 }
 
 function newFile() {
@@ -22,6 +80,34 @@ function newFile() {
 }
 
 function importClassDiagramFromJSON(result) {
-	console.log(result);
-	loaded = true;
+	let c = 0;
+	for (let thingy of result.split("\n")[0].split(",")) {
+		this.grid[c] = [];
+		c++;
+	}
+
+	let y = 0;
+	for (let line of result.split("\n")) {
+		let x = 0;
+		for (let thingy of line.split(",")) {
+			this.grid[x][y] = Number.parseInt(thingy);
+			x++;
+		}
+		y++;
+	}
+
+	this.loaded = true;
+}
+
+function keyPressed(){
+	if(key == "W"){
+		console.log("W");
+	}else if(key == "S"){
+
+	}else if(key == "A"){
+
+	}else if(key == "D"){
+
+	}
+	
 }
